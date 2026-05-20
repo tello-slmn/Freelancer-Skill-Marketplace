7ba0c19988b39af0fa52885f9cfecb1235e9a551
tello-slmn <tellosalman73@gmail.com>
2026-05-20 20:16:45 +0200

    #

---
+++ FSM/FSM/Controllers/SkillController.cs
@@ -0,0 +1,20 @@
+﻿using FSM.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FSM.Controllers
+{
+    public class SkillController : Controller
+    {
+        public IActionResult Details(int id)
+        {
+            var skill = DB.GetSkill(id);
+            return View(skill);
+        }
+
+        public IActionResult Index()
+        {
+            var skills = DB.GetSkills();
+            return View(skills);
+        }
+    }
+}
---
+++ FSM/FSM/Models/Skill.cs
@@ -0,0 +1,19 @@
+﻿namespace FSM.Models
+{
+    public class Skill
+    {
+        public int SkillID { get; set; }
+        public string SkillName { get; set; }
+        public Level SkillLevel { get; set; }
+
+        //User
+    }
+
+    public enum Level
+    {
+        Beginner,
+        Intermediate,
+        Advanced,
+        Expert
+    }
+}
